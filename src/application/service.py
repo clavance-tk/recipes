490906fcba6e56de7c97d69a684f5584eeec2cb0
clavance-tk <clavance.lim@travelperk.com>
2024-12-02 13:37:42 +0000

    tk create exercise

--- src/application/service.py
+++ src/application/service.py
@@ -1,41 +1,50 @@
 # Standard Library
-from typing import Dict, List, Union
+from typing import Any, Dict, List, Union
 
 # From apps
-from models.recipes import Recipe
+from models.recipes import Ingredient, Recipe
 from repository.errors import RepositoryError
 from repository.recipes import RecipesRepository, logger
 
 
 class RecipesService:
     @staticmethod
-    def get_recipe(recipe_id: str) -> Dict[str, Union[str, Recipe]]:
+    def get_recipe(recipe_id: str) -> Dict[str, Union[str, Dict]]:
         try:
             repository = RecipesRepository()
             recipe_data = repository.get_recipe(recipe_id)
 
-            if not isinstance(recipe_data, dict):
-                raise ValueError(f"Unexpected recipe format: {type(recipe_data)}")
+            # checks required because repository can return Recipe, dict, or error
+            if isinstance(recipe_data, dict) and "error" in recipe_data:
+                return {"status": "error", "message": recipe_data["message"]}
 
-            recipe = Recipe(**recipe_data)
-            return {"status": "success", "data": recipe}
+            if isinstance(recipe_data, dict):
+                if "ingredients" in recipe_data:
+                    recipe_data["ingredients"] = [Ingredient(**item) for item in recipe_data["ingredients"]]
+
+                recipe = Recipe(**recipe_data)
+            elif isinstance(recipe_data, Recipe):
+                recipe = recipe_data
+            else:
+                return {"status": "error", "message": "Unexpected data format from repository"}
+
+            return {"status": "success", "data": recipe.model_dump()}
 
         except RepositoryError as e:
             return {"status": "error", "message": str(e)}
 
         except Exception as e:
-            logger.error("Unexpected error in get_recipe: %s", e)
-            return {"status": "error", "message": "An unexpected error occurred."}
+            return {"status": "error", "message": f"An unexpected error occurred: {str(e)}"}
 
     @staticmethod
-    def create_recipe(data: dict) -> Dict[str, Union[str, Recipe]]:
+    def create_recipe(data: dict) -> Dict[str, Union[str, Dict[str, Any]]]:
         try:
             recipe_data = Recipe(**data)
 
             repository = RecipesRepository()
             result = repository.insert_recipe(recipe_data)
 
-            return {"status": "success", "data": result}
+            return {"status": "success", "data": result.model_dump()}
 
         except RepositoryError as e:
             return {"status": "error", "message": str(e)}
